ed9100364ed9daf99b99e2e87b569f044ee02fc5
smr8653 <90866434+smr8653@users.noreply.github.com>
2022-11-18 18:20:33 -0500

    Initial commit- drafting the PDA class

---
+++ PDA.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+// TODO 
+// import stack class
+// make state class
+// make this generic
+
+/**
+ * Simulates a pushdown automata.
+ */
+public class PDA {
+    private ArrayList<String> Q; // The set of states for the PDA
+    private ArrayList<String> sigma; // The set of input symbols
+    private String q0; // The initial state
+    private ArrayList<String> F; // The set of final states
+
+}
